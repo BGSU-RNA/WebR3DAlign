75a496e8c2695a449bbce9cd9daaade7fac80086
Anton Petrov <anton.i.petrov@gmail.com>
2012-12-25 12:03:58 -0500

    Template fix

--- application/config/r3dalign_template.php
+++ application/config/r3dalign_template.php
@@ -1,3 +1,8 @@
-$config = Array(
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array(
     'results_folder' => ''
 );
+
+/* End of file r3dalign.php */
+/* Location: ./application/config/r3dalign.php */
